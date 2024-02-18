ee0cc91d8fcb59dde9fff78ddbef44e7ffacc425
LorrEkk <63709739+LorrEkk@users.noreply.github.com>
2024-02-18 17:33:42 +0100

    draw and check requests

--- wordleServer/main.py
+++ wordleServer/main.py
@@ -1,15 +1,30 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy import func
+from sqlalchemy.orm import Session
+
+import database
+
+
+database.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
 
 
-# TODO
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @app.get("/wordle/draw")
-async def draw():
-    return "draw correct answer"
+def draw(db: Session = Depends(get_db)):
+    answer = db.query(database.Word).order_by(func.rand()).limit(1).first()
+    return answer.word
 
 
-# TODO
 @app.get("/wordle/check")
-async def check(word: str):
-    return "check answer"
+def check(word: str, db: Session = Depends(get_db)):
+    count = db.query(func.count()).filter(database.AllWords.word == word.upper()).scalar()
+    return count > 0
